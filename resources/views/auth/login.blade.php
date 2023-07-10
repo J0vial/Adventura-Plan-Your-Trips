c11faa61c35e5f333aa937d7b097e976ebc7c9c3
Md Shakibul Alam <70951378+J0vial@users.noreply.github.com>
2023-07-10 18:37:46 +0600

    css change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,11 @@
 @extends('default_template')
 @section('template')
-
-<link rel="stylesheet" href="{{ asset('auth/auth.css') }}">
+    <link href="{{ asset('home_page/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{ asset('home_page/css/fontawesome-all.css')}}" rel="stylesheet">
+    <link href="{{ asset('home_page/css/swiper.css')}}" rel="stylesheet">
+	<link href="{{ asset('home_page/css/magnific-popup.css')}}" rel="stylesheet">
+	<link href="{{ asset('home_page/css/styles.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('auth/auth.css') }}">
 </head>
 <body style="background-image: Radial-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)),url('home_page/images/header-background.jpg') ; background-size: fit">
     <nav class="navbar navbar-expand-md navbar-dark navbar-custom fixed-top">
@@ -9,7 +13,7 @@
         <!-- <a class="navbar-brand logo-text page-scroll" href="index.html">Aria</a> -->
 
         <!-- Image Logo -->
-        <h1 style="color: whitesmoke; position:relative; left:190px;">ADVENTURA</h1>
+        <h1 style="color:whitesmoke;font: 700 2.5rem/3rem "Montserrat", sans-serif;">ADVENTURA</h1>
         
         <!-- Mobile Menu Toggle Button -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,7 +22,7 @@
         </button>
         <!-- end of mobile menu toggle button -->
 
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault" style="position:relative; right:190px;">
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault" >
             <div class="overlay-panel overlay-right">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item" >
@@ -32,7 +36,7 @@
     </nav> <!-- end of navbar -->
 
 
-    <div class="container" id="container" style="min-height: 680px;">
+    <div class="container" id="container" style="min-height: 500px;">
         <div class="form-container sign-in-container">
             <form action="{{route('login-user')}}" method='post'>
                 @if(Session::has('success'))
@@ -61,8 +65,8 @@
             <div class="overlay">
                 
                 <div class="overlay-panel overlay-right">
-                    <h1>Are you NEW!</h1>
-                    <p>Enter your personal details and start journey with us</p>
+                    <h1 style="color: whitesmoke;">Have an Account!</h1>
+                    <p style="color: whitesmoke;">To enter your login details press here </p>
                     <button class="ghost" id="signUp" onclick="location.href='/register'">Sign Up</button>
                 </div>
             </div>
--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -1,7 +1,11 @@
 @extends('default_template')
 @section('template')
-
-<link rel="stylesheet" href="{{ asset('auth/auth.css') }}">
+    <link href="{{ asset('home_page/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{ asset('home_page/css/fontawesome-all.css')}}" rel="stylesheet">
+    <link href="{{ asset('home_page/css/swiper.css')}}" rel="stylesheet">
+	<link href="{{ asset('home_page/css/magnific-popup.css')}}" rel="stylesheet">
+	<link href="{{ asset('home_page/css/styles.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('auth/auth.css') }}">
 </head>
 <body style="background-image: Radial-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)),url('home_page/images/header-background.jpg'); background-size: fit">
     
@@ -12,7 +16,7 @@
             <!-- <a class="navbar-brand logo-text page-scroll" href="index.html">Aria</a> -->
 
         <!-- Image Logo -->
-        <h1 style="color: whitesmoke; position:relative; left:190px;">ADVENTURA</h1>
+        <h1 style="color:whitesmoke;font: 700 2.5rem/3rem "Montserrat", sans-serif;">ADVENTURA</h1>
         
         <!-- Mobile Menu Toggle Button -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,17 +25,17 @@
         </button>
         <!-- end of mobile menu toggle button -->
 
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault" style="position:relative; right:190px;">
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item" >
                     <a href="/">
-                        <button class="ghost" id="signUp" style="margin-top:30px" >  GO BACK TO HOMEPAGE</button>
+                        <button class="ghost" id="signUp">  GO BACK TO HOMEPAGE</button>
                     </a>
                 </li>
             </ul>
         </div>
     </nav> <!-- end of navbar -->
-    <div class="container" id="container" style="min-height: 680px;">
+    <div class="container" id="container" style="min-height: 550px;">
         <div class="form-container sign-in-container">
             <form action="{{route('register-user')}}" method="post">
                 @if(Session::has('success'))
@@ -69,8 +73,8 @@
             <div class="overlay">
                 
                 <div class="overlay-panel overlay-right">
-                    <h1>Have an Account!</h1>
-                    <p>To enter your login details press here </p>
+                    <h1 style="color: whitesmoke;">Have an Account!</h1>
+                    <p style="color: whitesmoke;">To enter your login details press here </p>
                     <button class="ghost" id="signUp" onclick="location.href='/login'">Sign In</button>
                     
                 </div>
